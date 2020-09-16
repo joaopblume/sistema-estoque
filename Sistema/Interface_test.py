4ade3ae7a699d8614bab86d4b5fce424665ad924
João Paulo Blume Vidal <joaopauloblume@gmail.com>
2020-09-16 01:08:22 -0300

    Excluir e vender commited
    Adicionada a função excluir, onde o registro do produto é excluido do banco de dados e da própria tabela de estoque.
    Adicionada a função vender; é selecionada a quantidade de produtos a ser vendido, é descontado do banco de dados e da tabela no estoque, e caso acabe o produto, é excluido do banco de dados e da tabela.

--- Sistema/Interface_test.py
+++ Sistema/Interface_test.py
@@ -1,6 +1,7 @@
 from PyQt5 import uic, QtWidgets
 import mysql.connector
 
+# conectando com localhost mysql
 banco = mysql.connector.connect(
     host='localhost',
     user='root',
@@ -8,13 +9,59 @@ banco = mysql.connector.connect(
     database='cadastro_produtos'
 )
 
+def excluir():
+    linha = storage.tableWidget.currentRow() 
+    id = storage.tableWidget.item(linha, 0).text()
+
+    cursor = banco.cursor()
+    comando = f'DELETE FROM produtos WHERE id = {id}'
+    cursor.execute(comando)
+    banco.commit()
+    
+    storage.tableWidget.removeRow(linha)
+
+def quantity():
+    sell.show()
+
+    linha = storage.tableWidget.currentRow()
+    total = int(storage.tableWidget.item(linha, 5).text())
+    sell.spinBox.setRange(1 , int(total))
+
+    sell.pushButton.clicked.connect(confirmar)
+
+    return total
+    
+
+def confirmar():
+
+    linha = storage.tableWidget.currentRow() 
+    id = storage.tableWidget.item(linha, 0).text()
+
+    valor = int(sell.spinBox.value())
+    resultado = quantity() - valor
+    if resultado == 0:
+        excluir()
+    else:
+        cursor = banco.cursor()
+        comando = f'UPDATE produtos SET quantidade = {resultado} WHERE id = {id}'
+        cursor.execute(comando)
+        banco.commit()
+        storage.tableWidget.setItem(linha, 5, QtWidgets.QTableWidgetItem(str(resultado)))
+    
+    sell.close()
+    return resultado
+
+
 def registrar():
+# captando os dados inseridos nas labels
     nome = new.lineEdit.text()
     tipo = new.comboBox.currentText()
     preco = new.lineEdit_2.text()
     codigo = new.lineEdit_3.text()
     quantidade = new.lineEdit_4.text()
-   
+
+
+# passando os dados captados para uma tabela na db
     cursor = banco.cursor()
     comando = 'INSERT INTO produtos (nome, tipo, preco, codigo, quantidade) VALUES (%s, %s, %s, %s, %s)'
     dados = (nome, tipo, str(preco), str(codigo), str(quantidade))
@@ -32,11 +79,15 @@ def novo_instrumento():
 def estoque():
     storage.show()
 
+    storage.tableWidget.verticalHeader().setVisible(False)
+
+# pegando todos os produtos já cadastrados no banco de dados
     cursor = banco.cursor()
     comando = 'SELECT * FROM produtos'
     cursor.execute(comando)
     dados_lidos = cursor.fetchall()
 
+# definindo tamanho das linhas e colunas da tabela
     storage.tableWidget.setRowCount(len(dados_lidos))
     storage.tableWidget.setColumnCount(6)
 
@@ -57,11 +108,21 @@ def estoque():
         for j in range(0, 6):
             storage.tableWidget.setItem(i, j, QtWidgets.QTableWidgetItem(str(dados_lidos[i][j])))
 
+# botão excluir e vender
+    storage.pushButton.clicked.connect(excluir)
+    storage.pushButton_2.clicked.connect(quantity)
+
+
 
 app = QtWidgets.QApplication([])
+
+# import das interfaces
 index = uic.loadUi('index.ui')
 new = uic.loadUi('novo_instrumento.ui')
 storage = uic.loadUi('estoque.ui')
+sell = uic.loadUi('vender.ui')
+
+# definindo funções para os botões de index.ui
 index.pushButton.clicked.connect(novo_instrumento)
 index.pushButton_2.clicked.connect(estoque)
 
